370558a4a4a16ad1b4c5b7413691dd759de4f9c6
George Camboim <george.camboim@embedded.ufcg.edu.br>
2020-08-07 00:49:59 -0300

    Solução do #9

---
+++ 009_pyth_triplet.py
@@ -0,0 +1,9 @@
+def main():
+    for b in range(500):
+        a = 500*(1000-2*b)/(1000-b)
+        if a.is_integer():
+            c = (a**2 + b**2) ** 0.5
+            print(int(a), b, int(c), int(a*b*c))
+
+if __name__ == "__main__":
+    main()
