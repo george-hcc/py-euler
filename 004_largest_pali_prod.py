786b2a1a9b4b27d6128dd0c94c6d40e1a995a2ef
George Camboim <george.camboim@gmail.com>
2023-07-26 08:37:15 -0300

    Optimizing the solution #4

--- 004_largest_pali_prod.py
+++ 004_largest_pali_prod.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 
-from tools.eratos_sieve import eratos_sieve
-import math
-
 def main():
     print(larg_pali_prod(100, 999))
 
 def larg_pali_prod(minor_n, major_n):
-    pali_prod = 0
-    for a in range(minor_n, major_n+1):
-        for b in range(a, major_n+1):
+    largest_pali = 0
+    for a in range(major_n, minor_n-1, -1):
+        for b in range(a, minor_n-1, -1):
             prod = a*b
-            if is_palindrome(prod) and prod > pali_prod:
-                pali_prod = prod
-    return pali_prod
+            if prod <= largest_pali:
+                break
+            if is_palindrome(prod):
+                largest_pali = prod
+    return largest_pali 
 
 def is_palindrome(number):
-    lst_number = list(str(number))
-    pivot = len(lst_number) // 2
-    return lst_number[0:pivot] == lst_number[-1:-(pivot+1):-1]
+    str_number = str(number)
+    return str_number == str_number[::-1]
     
 if __name__== '__main__':
     main()
